a6feedf0f2fef0e1abeb53656516f22b27ae4e3b
AND <andrey_g.pt@mail.ru>
2018-01-31 17:41:07 +0200

    data provider sample

--- calculator_test.php
+++ calculator_test.php
@@ -16,13 +16,24 @@ class CalculatorTests extends PHPUnit\Framework\TestCase
         $this->calculator = NULL;
     }
  
- 
+
     public function testAdd()
     {
         $result = $this->calculator->add(1, 2);
         $this->assertEquals(3, $result);
         $this->assertEquals(4, $result);
     }
- 
+
+    public function testAddWithZero()
+    {
+        $result = $this->calculator->add(0, 0);
+        $this->assertEquals(0, $result);
+    }
+
+    public function testAddWithNegative()
+    {
+        $result = $this->calculator->add(-1, -1);
+        $this->assertEquals(-2, $result);
+    }
 }
 ?>
